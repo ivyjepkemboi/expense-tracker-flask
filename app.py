ddda03df3ef4409839200d26d80684122a0a28fb
Ivy <ivyjkemboi@gmail.com>
2025-02-24 13:03:13 +0300

    render-port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -21,4 +22,7 @@ app.register_blueprint(expense_bp, url_prefix="/expenses")
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Create tables if they don't exist
-    app.run(debug=True)
+
+    # Get port from environment (Render assigns a port dynamically)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port, debug=True)
