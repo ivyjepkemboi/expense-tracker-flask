5edabd62f1910807d2b236e9589b596cb45a87ff
Ivy <ivyjkemboi@gmail.com>
2025-02-25 15:46:13 +0300

    changed to postgreSQL

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from config import Config
 from db import db
 from routes.auth_routes import auth_bp
 from routes.expense_routes import expense_bp
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,15 +15,12 @@ app.config.from_object(Config)
 db.init_app(app)
 CORS(app)
 jwt = JWTManager(app)
+migrate = Migrate(app, db)  # Initialize Flask-Migrate
 
 # Register Blueprints
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(expense_bp, url_prefix="/expenses")
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Create tables if they don't exist
-
-    # Get port from environment (Render assigns a port dynamically)
     port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
     app.run(host="0.0.0.0", port=port, debug=True)
--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///expenses.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "postgresql://expense_tracker_db_19k7_user:CRz1nz2t9sOPtoPNK8y7WTFvLUNAf0lM@dpg-cuur6dqj1k6c73a1897g-a.oregon-postgres.render.com/expense_tracker_db_19k7")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret')
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # Token expires in 1 hour
-
